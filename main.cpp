5e1061ab9c9ad3a982a9c03f96c57d1cea1d6fc2
suaybdemir <byausrimed@gmail.com>
2024-03-31 00:48:09 +0300

    Added 2 func and I deleted 1 func as well 1-Has deleted unnecessary logic of selected device disconnect. I think you oughtta disconnect through what you wanna disconnect. 2- I Added auto copy mac address process 3- Also has deleted manual copy and paste process.

--- main.cpp
+++ main.cpp
@@ -7,50 +7,116 @@ using namespace std;
 
 class Con{
     protected:
+
         string address;
+
     public:
+
         Con() : address(""){}
-        string& scan();
-        string& pairing();
-        string& connect();
-        string& disconnect();
+        void scanBluetoothDevices();
+        void pairing();
+        void connect();
+        void disconnect();
 };
 
-string& Con::scan()
+void Con::scanBluetoothDevices()
 {
+    address.clear(); // Firstly Clear Address
 
     cout<<"Scan Started"<<endl;
-    system("hcitool scan && hcitool scan > db.txt");
+    system("hcitool scan > db.txt");
 
-    cout<<"Do u wanna write MAC address[y/n]"<<endl;
-    char c; 
-    cin>>c;
-    if(c=='y')
+    fstream db;
+
+    db.open("db.txt",ios::in | ios::out);
+    if(db.fail())
     {
-        cout << "Write the Mac address" << endl;
-        cin>>address;
+        cout<<"Error #001 Do you want to try again[y/n]?"<<endl;
+        char ch;
+        cin>>ch;
+        if(ch=='y')
+        {
+            scanBluetoothDevices();
+        }
     }
-    return address;
+
+    int i = 0;
+
+    cout<<"-------------Appropriate Bluetooth Devices!!------------------- "<<endl;
+
+    string line;
+
+    while(getline(db,line))
+    {
+        if(i>=1)
+        {
+            cout<<i<<"- "<<line<<endl;
+        }
+        i++;
+    }
+
+    cout<<"Select an option (to exit write -1)"<<endl;
+    int lNum;
+    cin>>lNum;
+
+    lNum++;
+
+    string temp;
+    int count = 1;
+
+    db.clear(); // as to clearly run this program (cleared any error flags!)
+    db.seekg(0,ios::beg); // cursor came to begin
+
+    while(getline(db,line))
+    {
+        if(lNum==count)
+        {
+            temp = line;
+            cout<<line<<endl;
+            break;
+        }
+        count++;
+    }
+
+    for(int i = 0;i< temp.length()-17 ; i++)
+    {
+        if(temp[i+2]== ':')
+        {
+            for(int j = 0; j<17 ; j++)
+            {
+                address += temp[i+j];
+            }
+            cout<<endl;
+            break;
+        }
+    }
+
+    cout<<"This is selected Bluetooth Device MAC address!!!"<<address<<endl;
+
+    db.close();
+
+    if(db.fail())
+    {
+        cout<<"Error #002 ( Not important! )"<<endl;
+    }
 }
 
-
-
-string& Con::pairing() {
+void Con::pairing() {
     system(("bluetoothctl pair " + address).c_str());
     system("exit");
-    return address;
+    
 }
 
-string& Con::connect() {
+void Con::connect() {
     system(("bluetoothctl connect " + address).c_str());
     system("exit");
-    return address;
+    
 }
 
-string& Con::disconnect() {
+void Con::disconnect() {
     system("bluetoothctl disconnect ");
     system("exit");
-    return address;
+    
 }
 
 int main(void) {
@@ -80,12 +146,14 @@ int main(void) {
         cout << "----------5-Exit-------------------------" << endl;
         cout << "-----------------------------------------" << endl;
         cout << "-----------------------------------------" << endl;
+        // cout << "----------[Selected Mac Address]---------" << endl;
+        // cout << "---------"<<con.address<<"---------------" << endl;
         cout<<endl;
         int num;
         cin >> num;
         switch (num) {
             case 1:
-                con.scan();
+                con.scanBluetoothDevices();
                 break;
             case 2:
                 con.pairing();
